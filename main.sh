d38494e10bec5b0ed9fd27186d20ed0b9e4d68ef
ianalexmac <iamacdougall@alaska.edu>
2025-11-20 15:37:33 -0900

    change data output dir structure to nested

--- main.sh
+++ main.sh
@@ -3,12 +3,12 @@
 mkdir data
 mkdir data/deprecated
 
-# python src/xlsx2csv.py
+python src/xlsx2csv.py
 # python src/interties2grids.py
 # python src/codes2csv.py
 
 # python src/salesoperations.py
 
-./src/pull_metadata.sh
+# ./src/pull_metadata.sh
 
-aedg_metadata generate lookup_fuelcode_2023-11-08 -d raw/dowl --bbox none --time none --data-dictionary raw_dowl_data_dictionary.csv --save
+# aedg_metadata generate lookup_fuelcode_2023-11-08 -d raw/dowl --bbox none --time none --data-dictionary raw_dowl_data_dictionary.csv --save
