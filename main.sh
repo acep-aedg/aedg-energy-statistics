4a34a1310cfb334786cb68d73af55ed8976b71e5
ianalexmac <iamacdougall@alaska.edu>
2025-11-21 11:53:46 -0900

    test refactored metadata library

--- main.sh
+++ main.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-mkdir data
-mkdir data/deprecated
-
-python src/xlsx2csv.py
+# python src/xlsx2csv.py
 # python src/interties2grids.py
 # python src/codes2csv.py
 
-# python src/salesoperations.py
+python src/salesoperations.py
 
 # ./src/pull_metadata.sh
 
-# aedg_metadata generate lookup_fuelcode_2023-11-08 -d raw/dowl --bbox none --time none --data-dictionary raw_dowl_data_dictionary.csv --save
+aedg_metadata generate \
+    data/public_fuel_prices/public_fuel_prices.csv \
+    ~/repos/aedg-etl-2024/data-sources \
+    --bbox infer \
+    -t specify \
+    --save
