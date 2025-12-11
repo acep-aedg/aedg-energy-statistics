d8c2a692c0e6f750f24e0cd78e0b55496f5189d1
ianalexmac <iamacdougall@alaska.edu>
2025-12-10 16:43:27 -0900

    delete example data

--- main.sh
+++ main.sh
@@ -8,14 +8,6 @@ python src/salesoperations.py
 
 # ./src/pull_metadata.sh
 
-aedg_metadata generate \
-    data/public_fuel_prices/public_fuel_prices.csv \
-    ~/repos/aedg-etl-2024/data-sources \
-    -dd fields.csv \
-    --bbox infer \
-    -t specify 
-
-
 
 aedg_metadata generate \
     data/lookup_operator_2025-03-07/lookup_operator_2025-03-07.csv \
