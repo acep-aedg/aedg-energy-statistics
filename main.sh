b882e8c914957e23fac3146d89be2a8015320dc4
ianalexmac <iamacdougall@alaska.edu>
2025-12-10 16:40:12 -0900

    add config files, rename data dictionary

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# python src/xlsx2csv.py
+python src/xlsx2csv.py
 # python src/interties2grids.py
 # python src/codes2csv.py
 
@@ -11,6 +11,15 @@ python src/salesoperations.py
 aedg_metadata generate \
     data/public_fuel_prices/public_fuel_prices.csv \
     ~/repos/aedg-etl-2024/data-sources \
+    -dd fields.csv \
     --bbox infer \
-    -t specify \
-    --save
+    -t specify 
+
+
+
+aedg_metadata generate \
+    data/lookup_operator_2025-03-07/lookup_operator_2025-03-07.csv \
+    ~/repos/aedg-etl-2024/data-sources \
+    -dd lookup_data_dictionary.csv \
+    --bbox infer \
+    -t specify 
